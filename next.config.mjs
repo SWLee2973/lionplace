08656139a2b71a78e5d514100c58bea5ea2ab132
SWLee2973 <kek2973@naver.com>
2024-04-10 16:42:05 +0900

    [Init] 04/10 SWLee2973 github actions 이미지 처리 #0 - 이미지 리소스 호출 시 production 환경에서 경로 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  assetPrefix:
+    process.env.NODE_ENV === 'production'
+      ? 'https://swlee2973.github.io/lionplace'
+      : '',
+  images: {
+    loader: 'imgix',
+    path: 'https://swlee2973.github.io/lionplace',
+  },
 };
 
 export default nextConfig;
