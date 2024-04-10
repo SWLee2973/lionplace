78ec70bfa9c1ab1c1d2c3ad5ec70acf2dac55f56
상언 <46062634+SWLee2973@users.noreply.github.com>
2024-04-10 19:03:06 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const repo = 'lionplace';
+const assetPrefix = `/${repo}/`;
+const basePath = `/${repo}`;
+
 const nextConfig = {
   output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
     loader: 'custom',
     loaderFile: './src/lib/imageLoader.ts',
