b4da473eaa754a7b74d28bb915cbb72173b03891
SWLee2973 <kek2973@naver.com>
2024-04-13 15:29:52 +0900

    [Feat] 04/13 SWLee2973 로그인 마크업 #3 - 로고 컴포넌트 재사용 - 로그인 폼 마크업 - 로그인 관련 route group 레이아웃 분리

---
+++ src/components/level2/auth/LoginForm.tsx
@@ -0,0 +1,34 @@
+import LinkButton from '@/components/level1/auth/LinkButton';
+import LoginButton from '@/components/level1/auth/LoginButton';
+import FormInput from '@/components/level1/common/FormInput';
+import Separator from '@/components/level1/common/Separator';
+import Link from 'next/link';
+
+function LoginForm() {
+  return (
+    <form className="flex flex-col items-start gap-6">
+      <h2 className="text-heading-xl text-white">로그인</h2>
+      <div className="flex flex-col gap-3">
+        <FormInput inputType="text" placeholder="아이디를 입력해 주세요" />
+        <FormInput
+          inputType="password"
+          placeholder="비밀번호를 입력해 주세요"
+        />
+        <div className="flex items-center justify-end gap-2 text-label-small text-white">
+          <Link href="">아이디 찾기</Link>
+          <Separator />
+          <Link href="">비밀번호 찾기</Link>
+        </div>
+      </div>
+
+      <div className="mt-3 flex flex-col gap-3">
+        <LoginButton />
+        <LinkButton to="/register" theme="transparent">
+          회원가입
+        </LinkButton>
+      </div>
+    </form>
+  );
+}
+
+export default LoginForm;
