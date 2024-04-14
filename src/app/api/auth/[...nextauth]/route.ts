4c356fb8dca4f94721e9824d9f9c90b54a06911f
SWLee2973 <kek2973@naver.com>
2024-04-14 23:24:31 +0900

    [Feat] 04/14 SWLee2973 배포안되는 이슈 확인 #0 - authOptions 파일 분리

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,57 +1,5 @@
-import dbConnect from '@/dbConnect/dbConnect';
-import User from '@/models/User';
+import authOptions from '@/lib/authOptions';
 import NextAuth from 'next-auth/next';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import bcryptjs from 'bcryptjs';
-import { NextAuthOptions } from 'next-auth';
-
-export const authOptions: NextAuthOptions = {
-  secret: process.env.NEXTAUTH_SECRET,
-  pages: {
-    signIn: '/login',
-  },
-  session: {
-    strategy: 'jwt',
-    maxAge: 30 * 24 * 60 * 60,
-    updateAge: 24 * 60 * 60,
-  },
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        id: {},
-        password: {},
-      },
-
-      async authorize(credentials, req) {
-        const id = credentials?.id as string;
-        const plainPassword = credentials?.password as string;
-
-        await dbConnect();
-
-        const isUserExist = await User.findOne({ id: id });
-        if (!isUserExist) {
-          return null;
-        }
-
-        // 비밀번호 확인
-        const isValidPassword = await bcryptjs.compare(
-          plainPassword,
-          isUserExist?.password,
-        );
-        if (!isValidPassword) {
-          return null;
-        }
-
-        return {
-          userId: isUserExist?._id,
-          id: isUserExist?.id || 'Anonymous',
-        };
-      },
-    }),
-  ],
-  debug: process.env.NODE_ENV === 'development',
-};
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
