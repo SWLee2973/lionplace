81b3f98cf963b70958e1bb961f8b720786f90392
상언 <46062634+SWLee2973@users.noreply.github.com>
2024-04-15 00:01:22 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: 'default',
-  assetPrefix:
-    process.env.NODE_ENV === 'development'
-      ? ''
-      : 'https://swlee2973.github.io/lionplace/',
   images: {
     loader: 'custom',
     loaderFile: './src/lib/imageLoader.ts',
